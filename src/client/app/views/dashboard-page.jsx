c264114409fbdde3f0f60acce7782937164ec818
ASAF LEVY <asafl1976@gmail.com>
2016-05-08 23:04:22 +0300

    dashboard ui changes

--- src/client/app/views/dashboard-page.jsx
+++ src/client/app/views/dashboard-page.jsx
@@ -5,11 +5,13 @@ import Dropdown from 'muicss/lib/react/dropdown';
 import DropdownItem from 'muicss/lib/react/dropdown-item';
 import FixedWidthDropdown from './FixedWidthDropdown.jsx';
 import { strings } from '../constants/strings'
+import {Chart} from "react-chartjs";
+import {Bar} from "react-chartjs";
 
+var monthOptions = ["ינואר","פברואר","מרץ","אפריל","מאי","יוני","יולי","אוגוסט","ספטמבר","אוקטובר","נובמבר","דצמבר"];
 
 function getMonthName(monthNum)
 {
-    var monthOptions = ["ינואר","פברואר","מרץ","אפריל","מאי","יוני","יולי","אוגוסט","ספטמבר","אוקטובר","נובמבר","דצמבר"];
 
     if(monthNum > 12 || monthNum < 1)
     {
@@ -110,8 +112,36 @@ class DashboardCommissionChangeChart extends React.Component {
     }
 
     render () {
+
+        var data = {
+            labels: monthOptions,
+            datasets: [
+                {
+                    label: "My First dataset",
+                    backgroundColor: "rgba(215,49,132,0.2)",
+                    borderColor: "rgba(225,99,132,1)",
+                    borderWidth: 1,
+                    hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                    hoverBorderColor: "rgba(255,99,132,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    fillColor: "#4286b4"
+                }
+            ]
+        };
+        var chartOptions = {
+            scales: {
+                xAxes: [{
+                    stacked: true
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            }
+        }
+
         return (
             <div className="dashboard-commission-change-chart shadow">
+                <Bar data={data} options={chartOptions} width="600" height="200"/>
             </div>
         );
     }
